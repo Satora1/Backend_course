694023663a6e8e10b488ab27cbaf275a5b72309d
Jakub Satora <kubasatora@wp.pl>
2025-02-14 16:23:11 +0100

    backend play

---
+++ routes/auth.routes.js
@@ -0,0 +1,10 @@
+import { Route } from "express"
+const authRouter = Router();
+authRouter.post("/sign-up", (req, res) =>
+    res.send({ title: "Sign Up" }))
+authRouter.post("/sign-in", (req, res) =>
+    res.send({ title: "Sign In" }))
+authRouter.post("/sign-out", (req, res) =>
+    res.send({ title: "Sign Out" }))
+
+export default authRouter;
---
+++ routes/user.routes.js
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const userRouter=Router();
+userRouter.get("/",(req,res)=>res.send({title:"GET all users"}))
+userRouter.get("/:id",(req,res)=>res.send({title:"GET all users"}))
