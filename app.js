2374f427499da1d9f106ea3fd84335d2221bae12
Jakub Satora <kubasatora@wp.pl>
2025-02-17 16:31:40 +0100

    development mode and data base connection

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 import express from "express";
 import { PORT } from "./config/env.js";
+import userRouter from "./routes/user.routes.js";
+import authRouter from './routes/user.routes.js';
+import subscriptionRouter from "./routes/subscription.routes.js";
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+
 app.get("/", (req, res) => {
     res.send("welcome")
 })
 
-app.listen(PORT, () => {
-    console.log(`server runing: ${PORT} `)
+app.listen(PORT, async () => {
+    console.log(`server runing http://localhost:${PORT} `)
+    await connectToDatabase();
 })
 
 export default app;
