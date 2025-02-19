147d170a238c643110018e9fe3eeaeeb6623a4c3
Jakub Satora <kubasatora@wp.pl>
2025-02-19 15:00:01 +0100

    tested data and connection

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import { PORT } from "./config/env.js";
 import userRouter from "./routes/user.routes.js";
-import authRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,43 @@
-export const signUp=async(req,res,next)=>{
+import mongoose from "mongoose"
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken"
+import { JWT_SECRET, JWT_EXPIRES_IN } from '../config/env.js'
+export const signUp = async (req, res, next) => {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    try {
+        const { name, email, password } = req.body;
+        const existingUser = await User.findOne({ email })
+        if (existingUser) {
+            const error = new Error("User already exist");
+            error.statusCode = 409;
+            throw error;
+        }
+        //Hash password
+        const salt = await bcrypt.genSalt(10);
+        const hasedPassword = await bcrypt.hash(password, salt);
+        const newUser = await User.create([{ name, email, password: hasedPassword }], { session })
+        const token = jwt.sign({ userId: newUser[0]._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN })
+        await session.commitTransaction();
+        session.endSession()
+        res.status(201).json({
+            success: true,
+            messafe: "User created succsessfully",
+            data: {
+                token,
+                user: newUser[0]
+            }
+        })
+    } catch (error) {
+        await session.abortTransaction();
+        session.endSession();
+        next(error);
+    }
+}
+export const signIn = async (req, res, next) => {
 
 }
-export const signIn=async(req,res,next)=>{
+export const signOut = async (req, res, next) => {
 
-}
-export const signOut=async(req,res,next)=>{
-    
 }
