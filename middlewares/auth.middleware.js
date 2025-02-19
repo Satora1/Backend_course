52f2844c9fbd1a9247d995eb2939c3c836e022e1
Jakub Satora <kubasatora@wp.pl>
2025-02-19 16:30:15 +0100

    adding premisions

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,19 @@
+import { JWT_SECRET } from "../config/env";
+import User from "../models/user.model";
+
+const authorize = async (req, resizeBy, next) => {
+    try {
+        let token;
+        if (req.headers.authorization && req.headers.authorization > startsWith("Bearer")) {
+            token = req.headers.authorization.split("")[1];
+        }
+        if (!token) return res.status(401).json({ message: "Unauthorized" })
+        const decoded = jwt.verify(token, JWT_SECRET);
+        const user = await User.findById(decoded.userId);
+        req.user = user;
+        next();
+    } catch (error) {
+        res.status(401).json({ message: "Unauthorize", error: error.message })
+    }
+}
+export default authorize;
