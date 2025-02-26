108afe44f8026c167b7936eaa28937564edcf6f8
Jakub Satora <kubasatora@wp.pl>
2025-02-26 16:45:14 +0100

    fixed error now working

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,39 +1,47 @@
-import { workflowClient } from "../config/temp.js"
-import Subscription from "../models/subscription.model.js"
-import { SERVER_URL } from '../config/env.js'
-
+import { workflowClient } from "../config/temp.js";
+import Subscription from "../models/subscription.model.js";
+import { SERVER_URL } from "../config/env.js";
 
 export const createSubscription = async (req, res, next) => {
     try {
+        if (!workflowClient) {
+            throw new Error("workflowClient is not defined");
+        }
+
         const subscription = await Subscription.create({
             ...req.body,
             user: req.user._id,
         });
-        await workflowClient.trigger({
+
+        const { workflowRunId } = await workflowClient.trigger({
             url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
             body: {
                 subscriptionId: subscription.id,
             },
             headers: {
-                "content-type": "aplication/json",
+                "content-type": "application/json",
             },
             retries: 0,
-        })
-        res.status(201).json({ success: true, data: subscription })
-    } catch (e) {
-        next(e)
-    }
-}
-export const getUserSubscriptions = async (req, res, next) => {
-    try {
-        if (req.user.id !== req.params.id) {
-            const error = new Error("You are not the owner of this account")
-            error.status = 401;
-            throw error;
-        }
-        const subscriptions = await Subscription.find({ user: req.params.id })
-        res.status(200).json({ success: true, data: subscriptions })
+        });
+
+        res.status(201).json({ success: true, data: subscription ,workflowRunId});
     } catch (e) {
         next(e);
     }
-}
+};
+
+export const getUserSubscriptions = async (req, res, next) => {
+    try {
+        if (req.user.id !== req.params.id) {
+            return res.status(401).json({
+                success: false,
+                message: "You are not the owner of this account",
+            });
+        }
+
+        const subscriptions = await Subscription.find({ user: req.params.id });
+        res.status(200).json({ success: true, data: subscriptions });
+    } catch (e) {
+        next(e);
+    }
+};
