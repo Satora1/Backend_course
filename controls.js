b95624172f459904f0d79bec9781a64dc7f6a5ba
Jakub Satora <kubasatora@wp.pl>
2025-02-27 16:19:55 +0100

    endpoints

---
+++ controls.js
@@ -0,0 +1,24 @@
+// localhost:5500/api/v1/subscriptions
+// {
+// "name": "NFL",
+// "price": 35.99,
+// "currency": "USD",
+// "frequency": "weekly",
+// "category": "entertainment",
+// "startDate": "2025-02-25T00:00:00.000Z",
+// "paymentMethod": "Credit Card"
+// }
+
+//localhost:5500/api/v1/users/XXX
+// {
+//     "name": "NFL",
+//     "price": 35.99,
+//     "currency": "USD",
+//     "frequency": "weekly",
+//     "category": "entertainment",
+//     "startDate": "2025-02-25T00:00:00.000Z",
+//     "paymentMethod": "Credit Card"
+//     }
+
+//localhost:5500/api/v1/auth/sign-up
+//localhost:5500/api/v1/auth/sign-in
